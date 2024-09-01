224ba6c7bfcd6380a15bd6ec51fba3d84ab8f19d
guskirb <anguskirby1998@gmail.com>
2024-09-01 23:25:55 +0100

    normaliseUrl & tests

--- src/crawl.ts
+++ src/crawl.ts
@@ -1,4 +1,9 @@
 export function normaliseUrl(url: string) {
   const urlObj = new URL(url);
-  return `${urlObj.hostname}${urlObj.pathname}`;
+  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+
+  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+    return hostPath.slice(0, -1);
+  }
+  return hostPath;
 }
--- src/test/crawl.test.ts
+++ src/test/crawl.test.ts
@@ -1,9 +1,30 @@
 import { normaliseUrl } from "../crawl";
 import { test, expect } from "@jest/globals";
 
-test("normaliseUrl, stop protocol", () => {
+test("normaliseUrl strip protocol", () => {
   const input = "https://google.com";
   const output = normaliseUrl(input);
-  const expected = "google.com/";
+  const expected = "google.com";
+  expect(output).toEqual(expected);
+});
+
+test("normaliseUrl strip last /", () => {
+  const input = "https://google.com/hi/";
+  const output = normaliseUrl(input);
+  const expected = "google.com/hi";
+  expect(output).toEqual(expected);
+});
+
+test("normaliseUrl capitals", () => {
+  const input = "https://GOOGLE.com";
+  const output = normaliseUrl(input);
+  const expected = "google.com";
+  expect(output).toEqual(expected);
+});
+
+test("normaliseUrl strip http", () => {
+  const input = "http://google.com";
+  const output = normaliseUrl(input);
+  const expected = "google.com";
   expect(output).toEqual(expected);
 });
